6d93c390fa71dc9ec89dc6d1160646c03bb0e603
Moon <191705595@qq.com>
2017-04-20 19:23:29 +0800

    遗漏了dll.config的发布配置提交

---
+++ build/webpack.dll.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+console.log('dirname: ', __dirname)
+module.exports = {
+  entry: {
+    "dll_vendor": ['echarts']
+  },
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name]_[hash].dll.js',
+    /**
+     * output.library
+     * 将会定义为 window.${output.library}
+     * 在这次的例子中，将会定义为`window.vendor_library`
+     */
+    library: '[name]_library'
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      /**
+       * path
+       * 定义 manifest 文件生成的位置
+       * [name]的部分由entry的名字替换
+       */
+      path: path.join(__dirname, '../dist', '[name]-[hash]-manifest.json'),
+      /**
+       * name
+       * dll bundle 输出到那个全局变量上
+       * 和 output.library 一样即可。
+       */
+      name: '[name]_library',
+      context: path.join(__dirname, '../')
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+    sourceMap: true
+  })
+    ]
+};
